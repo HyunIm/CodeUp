c7b6905f1a1c64698ea88a5cfdd397b9e27528d9
Lim Hyun <hyunzion@gmail.com>
2021-05-29 20:03:27 +0900

    Add Conversion Problem

---
+++ Problem/6025.py
@@ -0,0 +1,8 @@
+# File : 6025.py
+# Dev : LimHyun (hyunzion@gmail.com)
+# Since : 2021-05-29
+# Brief : 변환
+
+a, b = map(int, input().split())
+
+print(a + b)
---
+++ Problem/6026.py
@@ -0,0 +1,9 @@
+# File : 6026.py
+# Dev : LimHyun (hyunzion@gmail.com)
+# Since : 2021-05-29
+# Brief : 변환
+
+a = float(input())
+b = float(input())
+
+print(a + b)
