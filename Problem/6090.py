97648ae9387c2e18cd8f7afdd804a787234498af
Lim Hyun <hyunzion@gmail.com>
2021-06-08 23:21:00 +0900

    Update 6090.py
    수열의 일반항으로 푸는 방법 추가 (주석)
      - Decimal : 정확한 나누기를 위해서 사용

--- Problem/6090.py
+++ Problem/6090.py
@@ -3,11 +3,12 @@
 # Since : 2021-06-07
 # Brief : 종합
 
+from decimal import Decimal
+
 a, m, d, n = map(int, input().split())
 
 for _ in range(0, n - 1):
     a = a * m + d
 print(a)
 
-# print(round(((a - (d / (1 - m))) * (m ** (n - 1))) + (d / (1 - m))) if m != 1 else (a + (d * (n - 1))))
-# print(round(((m**n)*(a*m+d-a)-d*m)/((m**2)-m)))
+# print((((m**n)*(a*m+d-a)-d*m)/Decimal((m**2)-m)) if m!=1 else (a+(d*(n-1))))
