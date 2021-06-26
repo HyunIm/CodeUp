75dd5a76c68166b4bb2dec7c80e281d505464066
Lim Hyun <hyunzion@gmail.com>
2021-06-26 22:58:46 +0900

    Add 2D Array Problem

---
+++ Problem/1488.py
@@ -0,0 +1,24 @@
+# File : 1488.py
+# Dev : LimHyun (hyunzion@gmail.com)
+# Since : 2021-06-26
+# Brief : 2차원 배열
+
+n, m = map(int, input().split())
+a = [[0]*m for _ in range(n)]
+i, j, c, k = n-1, m, 0, -1
+
+while n and m:
+    for _ in range(m):
+        j += k
+        c += 1
+        a[i][j] = c
+    n -= 1
+    for _ in range(n):
+        i += k
+        c += 1
+        a[i][j] = c
+    m -= 1
+    k = -k
+
+for i in a:
+    print(*i)
