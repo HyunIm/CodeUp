5e31e0447c7b18dcf5aed661a01661519db74f92
Lim Hyun <hyunzion@gmail.com>
2021-08-04 00:07:10 +0900

    Add Stack Problem

---
+++ Problem/1714.py
@@ -0,0 +1,6 @@
+# File : 1714.py
+# Dev : LimHyun (hyunzion@gmail.com)
+# Since : 2021-08-04
+# Brief : 스택
+
+print(input()[::-1])
---
+++ Problem/2016.py
@@ -0,0 +1,7 @@
+# File : 2016.py
+# Dev : LimHyun (hyunzion@gmail.com)
+# Since : 2021-08-04
+# Brief : 스택
+
+n=input()
+print(format(int(input()),','))
