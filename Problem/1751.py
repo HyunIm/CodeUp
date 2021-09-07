bf021cd7f08f4507e8a3f62424f21ee4013a6c26
Lim Hyun <hyunzion@gmail.com>
2021-09-07 21:07:43 +0900

    Add Basic Problem

---
+++ Problem/1751.py
@@ -0,0 +1,6 @@
+# File : 1751.py
+# Dev : LimHyun (hyunzion@gmail.com)
+# Since : 2021-09-07
+# Brief : 기초
+
+print(len(input()))
---
+++ Problem/1752.py
@@ -0,0 +1,6 @@
+# File : 1752.py
+# Dev : LimHyun (hyunzion@gmail.com)
+# Since : 2021-09-07
+# Brief : 기초
+
+print(input()[::-1])
