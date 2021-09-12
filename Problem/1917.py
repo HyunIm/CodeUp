880450939da8eeadd0ef1277a9e30e22eec62b04
Lim Hyun <hyunzion@gmail.com>
2021-09-13 01:17:45 +0900

    Add Basic Problem

---
+++ Problem/1917.py
@@ -0,0 +1,7 @@
+# File : 1917.py
+# Dev : LimHyun (hyunzion@gmail.com)
+# Since : 2021-09-13
+# Brief : 기초
+
+n, k = map(int, input().split())
+print(n**k)
---
+++ Problem/1919.py
@@ -0,0 +1,6 @@
+# File : 1919.py
+# Dev : LimHyun (hyunzion@gmail.com)
+# Since : 2021-09-13
+# Brief : 기초
+
+print(int(input()[::-1]))
