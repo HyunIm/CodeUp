c0817da133fa1f6c5ebe5a5ee61455e6ac1e7330
Lim Hyun <hyunzion@gmail.com>
2021-09-20 14:49:14 +0900

    Add Intermediate Problem

---
+++ Problem/2003.py
@@ -0,0 +1,7 @@
+# File : 2003.py
+# Dev : LimHyun (hyunzion@gmail.com)
+# Since : 2021-09-20
+# Brief : 중급
+
+k=int(input())
+print(('*'*k+'x'*k+'*'*k+'\n')*k+(' '*k+'xx'*k+'\n')*k+('*'*k+' '*k+'*'*k+'\n')*k)
